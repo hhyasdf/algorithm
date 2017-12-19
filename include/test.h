4d5b0abaa625a425f4268052a59c3ac4643035ac
hhy <552483776@qq.com>
2017-12-19 23:02:46 +0800

    create test.h

--- include/test.h
+++ include/test.h
@@ -1,10 +1,22 @@
 #include <iostream>
 
-#define TEST_SEQ_EQUAL(answer, result, size) \
-for (int i = 0; i <= size; ++i) {\
-}
+/*
+ * asume the answer and result sequence have size() function
+ */
+#define TEST_SEQ_EQUAL(target, result) \
+do { \
+    if(target.size() != result.size()) { \
+        std::cout << "size error!" << std::endl; \
+    } else { \
+        auto seq_size = target.size(); \
+        for (int i = 0; i <= seq_size; ++i) { \
+            \
+        } \
+    } \
+}while(0)
 
-#define TEST_NUM_EQUAL(answer, result) \
+
+#define TEST_NUM_EQUAL(target, result) \
 do{\
     std::cout << "target: " << answer << std::endl \
     std::cout << "result: " << result << std::endl \
