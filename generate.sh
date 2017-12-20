8cd0d6066f6309226e529553b0044d4b6e4e8162
hhy <552483776@qq.com>
2017-12-20 14:26:21 +0800

    add problem generate directory

---
+++ generate.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# to generate the directory of problem
+# use command line "./generate,.sh problemname" to generate
+
+if [ $# == 0 ]; then
+    echo "error: you should have specified the problem name!"
+
+else 
+    for i in $@; do
+        cp -r ./code_template/example ./problem/$i
+    done
+fi
