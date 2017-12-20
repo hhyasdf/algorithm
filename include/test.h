286413024f52bdb0702135507d9219b114d90171
hhy <552483776@qq.com>
2017-12-20 13:29:21 +0800

    add .gitignore and finish test.h

--- include/test.h
+++ include/test.h
@@ -1,7 +1,10 @@
 #include <iostream>
 
+#ifndef SIMPLE_TEST_HEAD
+#define SIMPLE_TEST_HEAD
+
 /*
- * asume the answer and result sequence have size() function
+ * asume the answer and result sequence have size() method
  */
 #define TEST_SEQ_EQUAL(target, result) \
 do { \
@@ -9,16 +12,55 @@ do { \
         std::cout << "size error!" << std::endl; \
     } else { \
         auto seq_size = target.size(); \
-        for (int i = 0; i <= seq_size; ++i) { \
-            \
+        bool flag = true; \
+        \
+        std::cout << "target sequence: ["; \
+        for (int i = 0; i < seq_size; ++i) { \
+            auto temp = *(target.begin() + i); \
+            if (i != seq_size - 1) { \
+                std::cout << temp << ", "; \
+            } else { \
+                std::cout << temp; \
+            } \
         } \
+        std::cout << "]" << endl; \
+        \
+        std::cout << "result sequence: ["; \
+        for (int i = 0; i < seq_size; ++i) { \
+            auto temp = *(result.begin() + i); \
+            if (*(target.begin() + i) != temp) { \
+                std::cout << "\033[31m" << temp << "\033[0m" << ", "; \
+                flag = false; \
+            } else { \
+                if (i != seq_size - 1) { \
+                    std::cout << temp << ", "; \
+                } else { \
+                    std::cout << temp; \
+                } \
+            } \
+        } \
+        std::cout << "]" << endl; \
+        std::cout << "answer is " << boolalpha << flag << std::endl; \
+        std::cout << endl; \
     } \
 }while(0)
 
-
-#define TEST_NUM_EQUAL(target, result) \
+/*
+ * string or number test 
+ * the target and result is a single value that support = operation
+ */
+#define TEST_VAL_EQUAL(target, result) \
 do{\
-    std::cout << "target: " << answer << std::endl \
-    std::cout << "result: " << result << std::endl \
-    std::cout << "answer is: " << (answer == result) << std::endl \
+    std::cout << "target value: " << target << std::endl; \
+    if (target != result) { \
+        std::cout << "result value: " << "\033[31m" << result << "\033[0m" << std::endl; \
+    } else { \
+        std::cout << "result value: " << result << std::endl; \
+    } \
+    std::cout << "answer is " << boolalpha << (target == result) << std::endl; \
+    std::cout << endl; \
 }while(0)
+
+#define START_TEST() int main()
+
+#endif
