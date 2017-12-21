e5fc746a9ce5fe3b47ee4ea39be2a11f112dadd7
hhy <552483776@qq.com>
2017-12-21 09:36:17 +0800

    modified code template

--- code_template/example/test.cpp
+++ code_template/example/test.cpp
@@ -22,13 +22,16 @@ START_TEST(){
 
     auto sample_input_begin = input.begin();
     auto sample_output_begin = output.begin();
-    
+
     for (int i = 0; i < sample_size; ++i) {
         cout << "Test " << i + 1 << ":" << endl;
 
-        PRINT_SEQ(*(sample_input_begin + i), "input: ");
+        // PRINT_SEQ(*(sample_input_begin + i), "input: ");
         // PRINT_VAL(*(sample_input_begin + i), "input: ");
 
+        // TEST_VAL_EQUAL(*(sample_output_begin + i), solution.algorithm(*(sample_input_begin + i)));
+        // TEST_SEQ_EQUAL(*(sample_output_begin + i), solution.algorithm(*(sample_input_begin + i)));
+        
         /*
          * execute the algorithm
          */
