5f521ea7ed53452ad4c880ed8b6ddef66c90bbe8
hhy <552483776@qq.com>
2017-12-30 11:06:47 +0800

    solved problem RangeSumQuery-Immutable

---
+++ problems/RangeSumQuery-Immutable/solution.h
@@ -0,0 +1,18 @@
+#include <vector>
+#include <string>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <set>
+
+using namespace std;
+
+/*
+ * the implement of algorithm
+ */
+
+class Solution {
+
+};
