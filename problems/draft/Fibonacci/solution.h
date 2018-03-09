82b0cc6f49eb16c6f7a5c94d9bf0b0fb4e29687c
hhyasdf <552483776@qq.com>
2018-03-09 22:47:03 +0800

    finish Fibonacci

---
+++ problems/draft/Fibonacci/solution.h
@@ -0,0 +1,44 @@
+#include <vector>
+#include <string>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+/*
+ * the implement of algorithm
+ */
+
+class Solution {
+public:
+    /**
+     * @param n: an integer
+     * @return: an ineger f(n)
+     */
+    int fibonacci(int n) {
+        // write your code here
+        int tmp1, tmp2, target;
+        tmp1 = 0;
+        tmp2 = 1;
+
+        if (n == 1) {
+            return tmp1; 
+        } else if (n == 2) {
+            return tmp2;
+        }
+
+        for (int i = 0; i < n - 2; i++) {
+            target = tmp1 + tmp2;
+            tmp1 = tmp2;
+            tmp2 = target;    
+        }
+
+        return target;
+    }
+};
